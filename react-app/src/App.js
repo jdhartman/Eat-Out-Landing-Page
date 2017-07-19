98a259299cd0291ed01d4476bb6f5212d8326de2
Todd Griffin <nemagnusfrater@gmail.com>
2017-07-18 21:53:56 -0500

    Update Header component.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,6 +15,98 @@ class App extends Component {
         <EatOut />
 
         <Founders />
+
+        <a href="http://eatout.trofi.io/" target="_blank" style="text-decoration: none;">
+          <div id="eo-div">
+            <div id="eo-plate">
+              <img src="images/plate.png" />
+            </div>
+
+            <div id="eo-ios"> 
+              <img src="images/iOS-mockup.png" />
+            </div>
+
+            <div id="eo-content">
+              <div id="eo-logo">
+                <img src= "images/Eat Out Logo.png" />
+              </div>
+
+              <div id="eo-motto">
+                Let's eat already.
+              </div>
+
+              <div id="eo-info">
+                A cross-platform restaurant finder that is coming soon to iOS and Android devices.
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <div id="about-div">
+          <p id="about-title">Who We Are</p>
+
+          <ul>
+            <li className="profile_pics">
+              <a href="http://justinhartman.me/">
+                <img src="images/portraits/justin.png" />
+              </a>
+
+              <a href="http://justinhartman.me/">
+                <p className="portrait_name">
+                  Justin Hartman
+                </p>
+              </a>
+
+              <p className="portrait_description">
+                Co-founder / iOS Developer
+              </p>
+            </li>
+
+            <li className="profile_pics">
+              <img src="images/portraits/theo.png" />
+
+              <p className="portrait_name">
+                Theo Burkhart
+              </p>
+
+              <p className="portrait_description">
+                Co-founder / iOS Developer
+              </p>
+            </li>
+
+            <li className="profile_pics">
+              <a href="http://magnusfrater.net/">
+                <img src="images/portraits/todd.jpg" />
+              </a>
+
+              <a href="http://magnusfrater.net/">
+                <p className="portrait_name">
+                  Todd Griffin
+                </p>
+              </a>
+
+              <p className="portrait_description">
+                Co-founder / Android Developer
+              </p>
+            </li>
+
+            <li className="profile_pics">
+              <img src="images/portraits/glen.jpg" />
+
+              <p className="portrait_name">
+                Glen Eder
+              </p>
+
+              <p className="portrait_description">
+                Co-founder / Android Developer
+              </p>
+            </li>
+          </ul>
+
+          <a id="about-join" href="https://docs.google.com/forms/d/e/1FAIpQLScW0bi6960ahp8PqsmlLKOOUAc4V9MR-H77Rq2pFCjdp-yOWg/viewform" target="blank"> 
+            <p>Want to join our team?</p>
+          </a>
+        </div>
       </div>
     );
   }
--- react-app/src/Header.js
+++ react-app/src/Header.js
@@ -3,32 +3,32 @@ import React, { Component } from 'react'
 import TrofiLogo from './res/images/trofi-logo.png'
 
 import FacebookLogo from './res/images/FB-Logo.png'
-import IGLogo from './res/images/IG-Logo.png'
 import TwitterLogo from './res/images/Twitter-Logo.png'
+import IGLogo from './res/images/IG-Logo.png'
 
 class Header extends Component {
     render () {
         return (
             <div id="logo-div">
-                <img id="title-logo" src={TrofiLogo} />
+                <img id="title-logo" src="images/trofi-logo.png" />
 
                 <div id="social">
                     <ul>
                         <li>
                             <a href="https://www.facebook.com/Trofi-LLC-308796409540099/" id="facebook" target="_blank">
-                            <img src={FacebookLogo} />
+                                <img src={FacebookLogo} />
                             </a>
                         </li>
 
                         <li>
                             <a href="https://twitter.com/trofiLLC" id="twitter" target="_blank">
-                            <img src={TwitterLogo} />
+                                <img src={TwitterLogo} />
                             </a>
                         </li>
 
                         <li>
                             <a href="https://www.instagram.com/trofi_llc/" id="instagram" target="_blank">
-                            <img src={IGLogo} />
+                                <img src={IGLogo} />
                             </a>
                         </li>
                     </ul>
