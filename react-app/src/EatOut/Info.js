b02520b30bd9e2bc1f81e2cb062b669b9a57dfe3
Todd Griffin <nemagnusfrater@gmail.com>
2017-07-22 17:24:38 -0500

    Separate EatOut component's Info component css from global & EatOut component's css.

--- react-app/src/EatOut/Info.js
+++ react-app/src/EatOut/Info.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import './global.css'
+import './Info.css'
 
 const Info = () => {
     return (
-        <div id="eo-info">
+        <div className="Info" id="eo-info">
             A cross-platform restaurant finder that is coming soon to iOS and Android devices.
         </div>
     )
