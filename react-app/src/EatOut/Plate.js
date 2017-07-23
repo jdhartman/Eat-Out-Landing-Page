0cbb91c0a6f1e7379ad43587f4392e40e6ca44d4
Todd Griffin <nemagnusfrater@gmail.com>
2017-07-22 20:48:39 -0500

    Separate EatOut component's Plate component's css from global & EatOut component's css. Rename Plate component's css selector from #eo-plate to .Plate.

--- react-app/src/EatOut/Plate.js
+++ react-app/src/EatOut/Plate.js
@@ -3,10 +3,11 @@ import React from 'react'
 import Resource from '../res/images/plate.png'
 
 import './global.css'
+import './Plate.css'
 
 const Plate = () => {
     return (
-        <div id="eo-plate">
+        <div className="Plate">
             <img src={Resource} alt="plate" />
         </div>
     )
